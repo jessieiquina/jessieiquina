220c637a8a409d12484b2bf2c8d8d7e5c2215d5e
jessieiquina <jiquina653@gmail.com>
2026-06-20 00:34:44 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-/*!
-* Start Bootstrap - The Big Picture v5.0.6 (https://startbootstrap.com/template/the-big-picture)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
