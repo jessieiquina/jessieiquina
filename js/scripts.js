80741a8e2ae4eb3ebc76425e95d0baa9fcedb675
jessieiquina <jiquina653@gmail.com>
2026-06-20 00:18:02 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,16 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// This script adds a 'scrolled' class to the navbar when the user scrolls down
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    // Change 50 to a different number if you want the navbar to change 
+    // color sooner or later as you scroll
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
